7cd1b19876b61f790d1e4cd1871e6a62ba49def6
JhChoy <jchoyeclipse@gmail.com>
2018-05-10 11:42:54 +0900

    add whitelist

--- tokenAndCrowdsale/contracts/crowdsale/Crowdsale.sol
+++ tokenAndCrowdsale/contracts/crowdsale/Crowdsale.sol
@@ -6,9 +6,22 @@ import "../token/IERC20.sol";
 
 contract Crowdsale is Ownable{
 
+    /* Library */
+
     using SafeMath for uint256;
 
+
+    /* Enum and Structure */
+
     enum STATE {PREPARE, ACTIVE, FINISHED, FINALIZED, REFUND}
+
+    struct Whitelist{
+        bool isListed;
+        uint maxcap;
+    }
+
+
+    /* Constants */
 
     uint public constant rate = 10 * 1000;
     uint public constant HARD_CAP = 1000 ether;
@@ -16,14 +29,27 @@ contract Crowdsale is Ownable{
     uint public constant START_TIME = 1525791600000; // UTC 2018 May 8th Tuesday PM 3:00:00
     uint public constant END_TIME = 1526004000000; // UTC 2018 May 11st Friday AM 2:00:00
 
+
+
+    /* Global Variables */
+
     IERC20 public mToken;
     address public mTeamWallet;
     STATE mCurrentState = STATE.PREPARE;
 
+    mapping(address => Whitelist) mWhitelist;
     mapping(address => uint) mContributors;
     uint public mContributedTokens = 0;
 
+
+
+    /* Event */
+
     event TokenPurchase(address indexed _purchaser, address indexed _receiver, uint _tokens);
+    event RefundEthers(address indexed _receiver, uint _ethers);
+
+
+    /* Modifier */
 
     modifier period(STATE _state){
         require(mCurrentState == _state);
@@ -31,6 +57,9 @@ contract Crowdsale is Ownable{
     }
 
 
+
+    /* Functions */
+    // Constructor
     constructor(
         address _tokenAddress,
         address _teamWallet
@@ -38,6 +67,8 @@ contract Crowdsale is Ownable{
         mToken = IERC20(_tokenAddress);
         mTeamWallet = _teamWallet;
     }
+
+    // View functions
     function getCurrentSate() view external returns(string){
         if(mCurrentState == STATE.PREPARE){
             return "PREPARE";
@@ -57,6 +88,16 @@ contract Crowdsale is Ownable{
         return mContributors[_contributor];
     }
 
+
+    // Set Functions
+    function addWhitelist(address _whitelist, uint _maxcap) public onlyOwner{
+        require(mCurrentState < STATE.FINISHED);
+        mWhitelist[_whitelist].isListed = true;
+        mWhitelist[_whitelist].maxcap = _maxcap;
+    }
+
+
+    // State Functions
     function activeSale() public onlyOwner period(STATE.PREPARE) {
         require(mToken.balanceOf(address(this)) == mToken.totalSupply());
         mCurrentState = STATE.ACTIVE;
@@ -76,6 +117,8 @@ contract Crowdsale is Ownable{
         mCurrentState = STATE.REFUND;
     }
 
+
+    // Payable Functions
     function () payable public{
         buyTokens(msg.sender);
     }
@@ -84,7 +127,9 @@ contract Crowdsale is Ownable{
         require(_receiver != address(0));
         require(msg.value > 0);
         require(now >= START_TIME && now < END_TIME);
+        require(mWhitelist[_receiver].isListed);
         uint amount = msg.value;
+        require(mWhitelist[_receiver].maxcap >= mContributors[_receiver].add(amount));
         uint tokens = 0;
         if(address(this).balance >= HARD_CAP){
             //should pay back left ethers
@@ -101,12 +146,26 @@ contract Crowdsale is Ownable{
         _addContributors(_receiver, tokens);
     }
 
+
+    // Public Functions
+    function receiveTokens() public period(STATE.FINALIZED){
+        require(mContributors[msg.sender] > 0);
+        mToken.transfer(msg.sender, mContributors[msg.sender]);
+        delete mContributors[msg.sender];
+    }
+
+    function refund() public period(STATE.REFUND){
+        require(mContributors[msg.sender] > 0);
+        uint ethers = mContributors[msg.sender].div(rate);
+        msg.sender.transfer(ethers);
+        delete mContributors[msg.sender];
+        emit RefundEthers(msg.sender, ethers);
+    }
+
+
+    // private Functions
     function _addContributors(address _contributor, uint _additionalToken) private {
-        if(mContributors[_contributor] > 0) {
-            mContributors[_contributor] = mContributors[_contributor].add(_additionalToken);
-        } else {
-            mContributors[_contributor] = _additionalToken;
-        }
+        mContributors[_contributor] = _additionalToken;
         mContributedTokens += _additionalToken;
     }
     
@@ -117,19 +176,5 @@ contract Crowdsale is Ownable{
         mTeamWallet.transfer(address(this).balance);
         mToken.transfer(mTeamWallet, mToken.balanceOf(address(this)).sub(mContributedTokens));
     }
-
-
-    function receiveTokens() public period(STATE.FINALIZED){
-        require(mContributors[msg.sender] > 0);
-        mToken.transfer(msg.sender, mContributors[msg.sender]);
-        delete mContributors[msg.sender];
-    }
-
-    function refund() public period(STATE.REFUND){
-        require(mContributors[msg.sender] > 0);
-        msg.sender.transfer(mContributors[msg.sender].div(rate));
-        delete mContributors[msg.sender];
-    }
-
 }
 
